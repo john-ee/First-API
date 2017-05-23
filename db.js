c2290090c05042167a77df6b012d9cc4916fc2e1
John-Nathan <johnnathan.hill@list.lu>
2017-05-23 16:03:58 +0200

    API functionnal, the problem was due to a bad use of postman

--- db.js
+++ db.js
@@ -2,4 +2,11 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://root:password@ds149431.mlab.com:49431/jnapi')
 var conn = mongoose.connection;
 
-conn.on('error', console.error.bind(console, 'connection error:')); 
+conn.on('error', console.error.bind(console, 'connection error:'));
+conn.once('open', function callback() {
+  console.log("Open connection to Database");
+});
+
+exports.test = function(req,res) {
+  res.render('test');
+};
--- user/UserController.js
+++ user/UserController.js
@@ -8,22 +8,24 @@ var User = require('./User');
 // creates a new User
 router.post('/', function (req,res) {
   User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : req.body.password
-  },
-  function (err, user) {
-      if (err) return res.status(500).send("There was a problem adding the info to the database");
-      res.status(200).send(user);
-  });
+      name : req.body.name,
+      email : req.body.email,
+      password : req.body.password
+    },
+    function (err, user) {
+        if (err) {
+          return res.status(500).send("There was a problem adding the info to the database");
+        }
+        res.status(200).send(user);
+    });
 });
 
 // returns all Users
 router.get('/', function (req, res) {
   User.find({}, function (err, users) {
-    if (err) return res.status(500).send("There was a proleme finding the users.");
+    if (err) return res.status(500).send("There was a problem finding the users.");
     res.status(200).send(users);
-  })
-})
+  });
+});
 
 module.exports = router;
