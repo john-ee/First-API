497b41be076f8c92849ed0b4b4c24d721ca19715
John-Nathan <johnnathan.hill@list.lu>
2017-05-24 13:09:35 +0200

    When a someone adds new device, a software is always attached to it.

--- device/DeviceController.js
+++ device/DeviceController.js
@@ -6,16 +6,27 @@ router.use(bodyParser.urlencoded({ extended: true}));
 var Device = require('./Device');
 var Software = require('../software/Software');
 
+var software_id;
+var Blank;
 Software.findOne({ name: 'Blank'}, function (err, software) {
   if (err) console.log('Error searching for Blank.');
   if (!software) console.log('Blank not in DB.');
-  console.log(software.id + ' ' + software.name + ' ' + software.description);
+  console.log(software._id + ' ' + software.name + ' ' + software.description);
+  Blank = software;
 });
 
 router.post('/', function (req, res) {
+  console.log(req.body.software);
   if (!req.body.software) {
-    var software_id = Blank._id;
+    software_id = Blank._id;
     console.log('Using blank soft ' + Blank._id);
+  }
+  else {
+    Software.findOne({ name: req.body.software}, function (err, software) {
+      if (err || !software) software_id = Blank._id;
+      else software_id = software._id;
+      console.log('Soft id ' + software_id);
+    });
   }
   Device.create ({
       name : req.body.name,
@@ -30,7 +41,7 @@ router.post('/', function (req, res) {
   });
 });
 
-// returns all Users
+// returns all Devices
 router.get('/', function (req, res) {
   Device.find({}, function (err, devices) {
     if (err) return res.status(500).send("There was a problem finding the devices.");
